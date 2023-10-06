238b8fa7792c239a01a50e1011b16830bda2366e
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-06 16:38:41 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <MainSection />
+      <Category />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <nav>
+      <div className="logo">Flippy-Brain</div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contant Us</li>
+          <li>FAQs</li>
+          <li>
+            <i className="fa-solid fa-heart"></i>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function MainSection() {
+  return (
+    <div className="main-section">
+      <div className="left">
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-solid fa-laptop"></i>
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-regular fa-heart"></i>
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-solid fa-laptop"></i>
+      </div>
+      <div className="center">
+        <div>
+          <h1>
+            The <span>Fusion</span> of Cards and Words Flip, Spell, Triumph!
+          </h1>
+          <p>
+            <b>Flippy Brain:</b> Spell & Brain - Flip the cards, craft the
+            words, conquer the game. Are You up for the ultimate challenge?
+          </p>
+        </div>
+        <div>
+          <a href="https://github.com/Harrshhpattell">
+            <button className="btn-start">Start</button>
+          </a>
+        </div>
+        <div>
+          <img
+            style={{ borderRadius: "10px" }}
+            width="100%"
+            src="./images/kid playing.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="left">
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-solid fa-laptop"></i>
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-regular fa-heart"></i>
+        <i class="fa-regular fa-face-smile"></i>
+        <span></span>
+        <span></span>
+        <i class="fa-regular fa-face-smile"></i>
+      </div>
+    </div>
+  );
+}
+
+function Category() {
+  return (
+    <div className="category">
+      <div className="message-box">
+        <h3>New updates</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+          quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Consequatur, nisi!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a href="www.google.com">Learn More</a>
+      </div>
     </div>
   );
 }
